0090463aa09c59876dcb3aaa6b4c912ae6d372c0
Wolgang <kralicek.roman@gmail.com>
2023-07-31 20:04:13 +0200

    dfsdfasd

---
+++ 02js-values-variables/script.js
@@ -0,0 +1,16 @@
+console.log("========================");
+console.log("========START===========");
+console.log("========================");
+
+
+let test1  = 100;
+console.log(typeof test1);
+test1 = "hovno";
+console.log(typeof null);
+console.log("========================");
+
+var test2 = 100;
+
+const birth = 100;
+birth = 10;
+console.log(birth);
