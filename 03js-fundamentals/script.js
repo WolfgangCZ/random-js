c49564aaafd35d8efc8dd3dd2392fd1454a0e056
Wolgang <kralicek.roman@gmail.com>
2023-08-16 21:39:54 +0200

    next lecture

---
+++ 03js-fundamentals/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log("i can drive");
+*/
+
+function func(number)
+{
+    console.log(`fuck off ${number} times`);
+}
+
+func(50);
